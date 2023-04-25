9a50d5fe324efd1934e16ac7ed6a2495ce769fa1
Nawaritta <Nor.benhaddou@gmail.com>
2023-04-25 01:29:54 +0000

    modifiers.c and printf

---
+++ modifiers.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * print_width -  width modifier.
+ * @output: points to the buff structure
+ * @printed: printed characters.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int print_width(buffer_t *output, unsigned int printed,
+			 unsigned char flags, int wid)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 0)
+	{
+		for (wid -= printed; wid > 0;)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
+
+/**
+ * print_string_width - contains the leading spaces to a buff for a width modifier.
+ * @output: pointer to the buff.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @size: String's size.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int print_string_width(buffer_t *output,
+				unsigned char flags, int wid, int prec, int size)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 0)
+	{
+		wid -= (prec == -1) ? size : prec;
+		for (; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
+
+/**
+ * print_neg_width - Stores trailing space.
+ * @output: pointer to the buff.
+ * @printed: printed char.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+			     unsigned char flags, int wid)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 1)
+	{
+		for (wid -= printed; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
---
+++ printf.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * cleanup - cleans up for _printf.
+ * @args: arguments lists provided to _printf.
+ * @output: a buffer struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - checks the format string for printf function.
+ * @format: the string to print
+ * @output: containins the buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: number of characters stored.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			  unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+						&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - prints a formatted string.
+ * @format: String to print.
+ *
+ * Return: number of printed characters.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
+}

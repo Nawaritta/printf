070b4d2c97544f79a44efd4e20a73515cdaddc89
Damicable <annobidiegwu@gmail.com>
2023-04-26 16:42:50 -0700

    Updated

--- main.h
+++ main.h
@@ -17,9 +17,6 @@
 #define NEG_FLAG ((flags >> 4) & 1)
 #define SHORT 1
 #define LONG 2
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
 
 typedef unsigned int unsint;
 
@@ -110,20 +107,6 @@ unsigned int convert_r(va_list args, buffer_t *output,
 		       unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_R(va_list args, buffer_t *output,
 		       unsigned char flags, int wid, int prec, unsigned char len);
-
-int write_number(int is_negative, int ind, char buffer[],
-	int flags, int width, int precision, int length);
-int write_num(int ind, char buffer[],
-	int flags, int width, int prec,
-	int length, char padd, char extra_c);
-int write_unsgnd(int is_negative, int ind,
-	char buffer[],
-	int flags, int width, int precision, int length);
-nt write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int length)
-
 
 unsigned char handle_flags(const char *flags, char *index);
 unsigned char handle_length(const char *modifier, char *index);
